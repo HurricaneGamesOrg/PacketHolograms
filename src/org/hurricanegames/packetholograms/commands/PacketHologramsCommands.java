f8d75e8bad487d2c06874b450981763953a3119a
Shevchik <Shev4ik.den@gmail.com>
2020-02-21 14:53:24 +0300

    Fix remove command

--- src/org/hurricanegames/packetholograms/commands/PacketHologramsCommands.java
+++ src/org/hurricanegames/packetholograms/commands/PacketHologramsCommands.java
@@ -9,7 +9,7 @@ public class PacketHologramsCommands extends CommandRouter<PacketHologramsComman
 		super(helper);
 		addCommand("list", new PacketHologramsHologramListCommand(helper));
 		addCommand("add", new PacketHologramsHologramAddCommand(helper));
-		addCommand("remove", new PacketHologramsHologramAddCommand(helper));
+		addCommand("remove", new PacketHologramsHologramRemoveCommand(helper));
 		addCommand("move", new PacketHologramsHologramMoveCommand(helper));
 		addCommand("line", new PacketHologramsHologramLineCommands(helper));
 	}
